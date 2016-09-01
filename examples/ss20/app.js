922cc2abdd9b06eb6320698ecf002a7c17a61561
jayson temporas <iloveprogramming17@gmail.com>
2016-09-01 23:16:00 +0800

    test

--- examples/ss20/app.js
+++ examples/ss20/app.js
@@ -3,25 +3,18 @@
 	// configure our routes
 	app.config(function($routeProvider) {
 		$routeProvider
-
-	
 			.when('/home', {
 				templateUrl : 'pages/home.html',
 				controller  : 'mainController'
 			})
-
-			
 			.when('/about', {
 				templateUrl : 'pages/about.html',
 				controller  : 'aboutController'
 			})
-
-	
 			.when('/contact', {
 				templateUrl : 'pages/contact.html',
 				controller  : 'contactController'
 			})
-
 			 .otherwise({
       				redirectTo: '/home'
   			});
